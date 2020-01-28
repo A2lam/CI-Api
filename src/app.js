bb2915e3baa894b6b989417070a7813042c7e84b
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2020-01-28 16:11:58 +0100

    Implementing entry point of the api

---
+++ src/app.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import http from 'http';
+
+// Creating a new Express application
+const app = express();
+
+// Cors config
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+
+// global.__basedir = __dirname;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// app.listen(process.env.PORT);
+
+const server = http.createServer(app);
+
+server.listen(process.env.PORT);
+
+export default server;
